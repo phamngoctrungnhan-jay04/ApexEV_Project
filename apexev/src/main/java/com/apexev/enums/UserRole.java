a93000622f4b6a73afaca0a707e893c8d0887449
PhatPham14 <jicamelpham.ginious@gmail.com>
2025-10-17 20:28:17 +0700

    config database
    test create a table - code first (table user)

---
+++ apexev/src/main/java/com/apexev/enums/UserRole.java
@@ -0,0 +1,5 @@
+package com.apexev.enums;
+
+public enum UserRole {
+    CUSTOMER, ADMIN, BUSINESS_MANAGER, TECHNICIAN, SERVICE_ADVISOR
+}
---
+++ apexev/src/test/java/com/apexev/ApexevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apexev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApexevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
